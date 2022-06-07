acf7df629e61f11846af8e890ced0b43a4324b79
KainanGB <kainanytbr@gmail.com>
2022-06-07 18:32:49 -0300

    redoing most of the layout, used grid on projects section and rewrote mediaqueries

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const ul = document.querySelector(".links");
 const check = document.querySelector("#check");
+const nav = document.querySelector(".nav");
 
 check.addEventListener("click", () => {
+  nav.style.backgroundColor = "#171c28";
   if (ul.style.left === "0%") {
+    nav.style.backgroundColor = "transparent";
+
     ul.style.left = "100%";
   } else ul.style.left = "0%";
 });
 
 ul.addEventListener("click", (e) => {
+  nav.style.backgroundColor = "transparent";
   if ((e.target.className = "link")) ul.style.left = "100%";
 });
