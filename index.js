79eacdb074ed3ff9a61aaa97bdb5dc8636f4bb45
KainanGB <kainanytbr@gmail.com>
2022-06-08 15:26:57 -0300

    added experiences section and finished layout for cards section and added fade in animation on scroll

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
 const ul = document.querySelector(".links");
 const check = document.querySelector("#check");
 const nav = document.querySelector(".nav");
@@ -14,4 +17,32 @@ check.addEventListener("click", () => {
 ul.addEventListener("click", (e) => {
   nav.style.backgroundColor = "transparent";
   if ((e.target.className = "link")) ul.style.left = "100%";
+});
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
 });
